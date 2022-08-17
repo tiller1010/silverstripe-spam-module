25ec9e09d6061131ad0ec8731d5c60da46ca49bf
Tyler <tiller1010@gmail.com>
2022-08-17 14:27:31 -0400

    Another squash

--- src/Forms/TimerField.php
+++ src/Forms/TimerField.php
@@ -63,6 +63,7 @@ class TimerField extends TextField
       // Squash test
       // Squash test
       // Squash test
+      // Squash test
       // bugfix for 1.1.16
       //
         $attributes = [
